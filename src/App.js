1f3ca0b80e5671b86660a3e65b00d82cf1174f13
Nick <nhunsaker20@gmail.com>
2020-04-17 05:28:02 -0600

    add some pic

--- src/App.js
+++ src/App.js
@@ -7,14 +7,27 @@ import auth0 from './assets/auth0.png'
 import html from './assets/html.png'
 import reactIcon from './assets/react.png'
 import redux from './assets/redux.png'
+import apple from './assets/apple.png'
+import git from './assets/git.png'
+import ios from './assets/ios.png'
+import node from './assets/node_js.png'
+import nodemailer from './assets/nodemailer.png'
+import stripe from './assets/stripe.png'
+import sass from './assets/sass.png'
+import gitHub from './assets/github.png'
+import window from './assets/windows.png'
+import postgresql from './assets/postgresql.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} alt='ios'/>
+        {/* <img src={logo} alt='ios'/> */}
       </header>
+      <seciton className='intro'>
+        <h1>Nicholas K. Hunsaker</h1>
+      </seciton>
       <section className='skill-section'>
         <div className='skill-languages'>
           <div>
@@ -54,6 +67,56 @@ function App() {
             <img src={redux} alt='redux'/>
             <label>Redux</label>
           </div>
+        </div>
+
+        <div className='skill-languages-others'>
+          <div>
+            <h2>Others</h2>
+          </div>
+          <div className='flex-h'>
+          <div>
+            <img src={apple} alt='auth0'/>
+            <label>Mac</label>
+          </div>
+          <div>
+            <img src={window} alt='react'/>
+            <label>Window</label>
+          </div>
+          <div>
+            <img src={ios} alt='html-5'/>
+            <label>iOS</label>
+          </div>
+          <div>
+            <img src={node} alt='react'/>
+            <label>NodeJS</label>
+          </div>
+          <div>
+            <img src={postgresql} alt='react'/>
+            <label>PostgreSQL</label>
+          </div>
+          <div>
+            <div className="git-hub-background">
+              <img className="git-hub-img" src={gitHub} alt='react'/>
+            </div>
+            <label>GitHub</label>
+          </div>
+          <div>
+            <img src={git} alt='react'/>
+            <label>Git</label>
+          </div>
+          <div>
+            <img src={nodemailer} alt='redux' style={{backgroundColor: 'white', padding:'0 4px'}}/>
+            <label>Nodemailer</label>
+          </div>
+          <div>
+            <img src={stripe} alt='react'/>
+            <label>Stripe</label>
+          </div>
+          <div>
+            <img src={sass} alt='react'/>
+            <label>Sass</label>
+          </div>
+        </div>
         </div>
 
       </section>
